078eb85cd103b0a31c68b06a81fb1304a377793e
Pako Jack Motsumi <pjmotsumi06@gmail.com>
2026-03-04 10:33:19 +0200

    Added signup functionality with error handling

--- functions/src/routes/auth.ts
+++ functions/src/routes/auth.ts
@@ -12,3 +12,47 @@ interface LoginData {
     uid: string;
 }
 
+// Sign-up: Creating a new user
+
+// Defines a callable function (triggered by onCall)
+export const signup = functions.https.onCall<SignupData> (async (request) => {
+
+    // pulls specific fields out of the SignupData object for use by the server
+    const {email, password} = request.data;
+
+    // Validation check; Stops bad data 
+    if (!email || password.length < 6) {
+        throw new functions.https.HttpsError("invalid-argument", "Password must be at least 6 characters.");
+    }
+    
+    try {
+        // Instructs the firebase auth system to generate a new entry in the "Users" list
+        const user = await admin.auth().createUser({email, password});
+
+        // creates a user document in Firestore when new users sign up
+        await admin.firestore().collection("users").doc(user.uid).set({
+            // Create or override document with this data
+            email: user.email, // user email from auth
+            createdAt: admin.firestore.FieldValue.serverTimestamp(), // current server time
+            role: "user" // default role
+        });
+
+        // const redirect = {url: 'https://localhost:5000/login'};
+        // const link = await admin.auth().generateEmailVerificationLink(email, redirect);
+
+        // returns the uid and email if successfull
+        return {
+            uid: user.uid, 
+            email:user.email, 
+            // verificationLink: link
+            };
+
+    } catch (error: any) {
+        // Specific error mapping
+        if (error.code == 'auth/email-already-exists'){
+            throw new functions.https.HttpsError("already-exists", "Email is already registered.");
+        }
+
+        throw new functions.https.HttpsError("internal", "Something went wrong.");
+    }
+});
