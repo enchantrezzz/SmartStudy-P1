18ead95decf350b16e85abcefa8286badfdf7249
Pako Jack Motsumi <pjmotsumi06@gmail.com>
2026-03-05 17:24:56 +0200

    Updated interfaces to include displayName and modified the handlers accordingly

--- functions/src/routes/auth.ts
+++ functions/src/routes/auth.ts
@@ -3,6 +3,7 @@ import * as admin from "firebase-admin";
 
 // Defines what s sign-up request must look like
 interface SignupData {
+    displayName: string;
     email: string;
     password: string;
 }
@@ -15,63 +16,79 @@ interface LoginData {
 // Sign-up: Creating a new user
 
 // Defines a callable function (triggered by onCall)
-export const signup = onCall<SignupData> (async (request) => {
-
+export const signup = onCall<SignupData>(async (request) => {
     // pulls specific fields out of the SignupData object for use by the server
-    const {email, password} = request.data;
+    const { displayName, email, password } = request.data;
 
-    // Validation check; Stops bad data 
+    // Validation check; Stops bad data
     if (!email || !password || password.length < 6) {
-        throw new HttpsError("invalid-argument", "Password must be at least 6 characters.");
+        throw new HttpsError(
+            "invalid-argument",
+            "Password must be at least 6 characters.",
+        );
     }
-    
+
     try {
         // Instructs the firebase auth system to generate a new entry in the "Users" list
-        const user = await admin.auth().createUser({email, password});
+        const user = await admin.auth().createUser({ displayName, email, password });
 
         // creates a user document in Firestore when new users sign up
         await admin.firestore().collection("users").doc(user.uid).set({
             // Create or override document with this data
+            username: user.displayName,
             email: user.email, // user email from auth
             createdAt: new Date().toISOString(), // current server time
-            role: "user" // default role
+            role: "user", // default role
         });
+
+        const token = await admin.auth().createCustomToken(user.uid);
 
         // returns the uid and email if successfull
         return {
-            uid: user.uid, 
-            email:user.email, 
+            displayName: user.displayName,
+            uid: user.uid,
+            email: user.email,
+            token
             // verificationLink: link
-            };
-
+        };
     } catch (error: any) {
-
         console.log("Full error log: ", error.message);
         // Specific error mapping
-        if (error.code == 'auth/email-already-exists'){
+        if (error.code == "auth/email-already-exists") {
             throw new HttpsError("already-exists", "Email is already registered.");
         }
 
-        throw new HttpsError("internal", `Something went wrong, ERROR: ${error.message}`);
+        throw new HttpsError(
+            "internal",
+            `Something went wrong, ERROR: ${error.message}`,
+        );
     }
 });
 
-// Login: login an existing user 
+// Login: login an existing user
 
-export const login = onCall<LoginData> (async (request) => {
-
+export const login = onCall<LoginData>(async (request) => {
     // Pull field from LoginData object (data extraction)
-    const {uid} = request.data;
 
-    // Validation: check if a UID was provided
-    if (!uid) {
-        throw new HttpsError("invalid-argument", "User ID is required");
+    const token = request.auth?.token;
+
+    if (!token) {
+        throw new HttpsError(
+            "unauthenticated",
+            "User must be logged in."
+        );
     }
 
-    try {
+    const uid = token.uid;
+    const email = token.email;
 
-        // Fetch user profile from Firestore 
-        const userDoc = await admin.firestore().collection("users").doc(uid).get();
+    try {
+        // Fetch user profile from Firestore
+        const userDoc = await admin
+        .firestore()
+        .collection("users")
+        .doc(uid)
+        .get();
 
         // Check if user exists in the database
         if (!userDoc.exists) {
@@ -79,13 +96,16 @@ export const login = onCall<LoginData> (async (request) => {
         }
 
         const userData = userDoc.data();
+        const lastLogin = new Date().toISOString();
 
-        // return the profile data 
+        await admin.firestore().collection("users").doc(uid).update({lastLogin})
+
+        // return the profile data
         return {
-            uid: uid,
-            email: userData?.email,
+            uid,
+            email,
             role: userData?.role,
-            lastLogin: new Date().toISOString()
+            lastLogin,
         };
     } catch (error: any) {
         throw new HttpsError("internal", `Login failed: ${error.message}`);
