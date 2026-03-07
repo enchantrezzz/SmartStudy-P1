b26dcbf65a331dc75fd9029e8155e8599f0ed4f6
Pako Jack Motsumi <pjmotsumi06@gmail.com>
2026-03-07 14:49:06 +0200

    Updated file name and location in file structure

---
+++ functions/src/config/firebase.ts
@@ -0,0 +1,16 @@
+// imports the firebase cloud functions library
+// import * as functions from "firebase-functions";
+
+// imports the Firebase Admin SDK
+import * as admin from "firebase-admin";
+
+import {signup, login, resetPass} from "../routes/auth";
+
+// initializes the Firebase Admin SDK with default credentials
+admin.initializeApp();
+
+// Export functions so firebase can use them
+export const signupUser = signup;
+export const loginUser = login;
+export const resetUserPassword = resetPass;
+
